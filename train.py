833537c9a548d76a811e5d18cde826dfe942b6a0
Hyunji Kim <hji.kim@megazone.com>
2024-08-21 10:36:04 -0400

    imported os library

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from ultralytics import YOLO
 import cv2
+import os
 
 # Training
 def train_yolov8(data_path, model_path, trained_model_path, output_dir):
@@ -43,12 +44,16 @@ def test_yolov8(model_path, source, output_image_path):
     print(f"Result image saved to {output_image_path}")
 
 if __name__=='__main__':
+    cwd = os.getcwd()
     # Paths to your dataset and models
-    data_config = 'C:\\Users\\Kim\\Desktop\\GitHub\\personal-project-2\\data.yaml'
-    initial_model = 'C:\\Users\\Kim\\Desktop\\GitHub\\personal-project-2\\yolov8n.pt'  # Or any other YOLOv8 pre-trained model
-    final_model = 'C:\\Users\\Kim\\Desktop\\GitHub\\personal-project-2\\yolov8n_trained.pt'
-    output_dir = 'C:\\Users\\Kim\\Desktop\\GitHub\\personal-project-2\\runs\\train'
-    output_image_path = 'C:\\Users\\Kim\\Desktop\\GitHub\\personal-project-2\\test_result\\riped_tomato_97.jpeg'
+    data_config = os.path.join(cwd, 'data.yaml')
+    initial_model = os.path.join(cwd, 'yolov8n.pt')  # Or any other YOLOv8 pre-trained model
+    final_model = os.path.join(cwd, 'yolov8n_trained.pt')
+    output_dir = os.path.join(cwd, 'runs', 'train')
+
+    test_img_name = 'unriped_tomato_80.jpeg'
+    test_img_loc = os.path.join(cwd, 'dataset', 'test', 'images', test_img_name)
+    output_image_path = os.path.join(cwd, 'test_result', test_img_name)
 
     # Train the model
     #train_yolov8(data_config, initial_model, final_model, output_dir)
@@ -57,4 +62,4 @@ if __name__=='__main__':
     #validate_yolov8(final_model, data_config)
 
     # Test the model on a new image or video
-    test_yolov8(final_model, 'C:\\Users\\Kim\\Desktop\\GitHub\\personal-project-2\\dataset\\test\\images\\riped_tomato_97.jpeg', output_image_path)
+    test_yolov8(final_model, test_img_loc, output_image_path)
